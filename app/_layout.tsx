5945e7c9b7f397e023138b356b24a73e57f82647
eaejaum <jpedroalmeida179@gmail.com>
2025-01-16 21:12:31 -0300

    importing context to routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { AuthContextProvider } from "./context/authContext";
+import { CarsContextProvider } from "./context/carsContext";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="home" />
-      <Stack.Screen name="[carBrandCode]" />
-    </Stack>
+      <CarsContextProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="home" />
+          <Stack.Screen name="model/[carBrandCode]" />
+        </Stack>
+      </CarsContextProvider>
   );
 }
